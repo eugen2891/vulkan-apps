44eb98fc5017b7d6dca4dea3248b0cd9ed692a59
Eugen Brusilovskyy <eugen_b@mail.de>
2023-09-18 19:32:29 +0200

    SBRenderer: Color-fill basic application

--- projects/sbrenderer/main.c
+++ projects/sbrenderer/main.c
@@ -1,4 +1,8 @@
+#include <vkk.h>
 #include <SDL2/SDL.h>
+#include <cgltf/cgltf.h>
+
+#define CGLTF_IMPLEMENTATION
 
 static SDL_bool processWindowEvents();
 
@@ -8,17 +12,37 @@ int main(int argc, char** argv)
 	uint32_t windowFlags = SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN;
 	SDL_Window* window = SDL_CreateWindow("Sandbox Renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 800, windowFlags);
 
+	requestWindowSurface(window);
+	requestDefaultCommandQueue(3, true);
+	requestPresentMode(VK_PRESENT_MODE_FIFO_KHR);
+	requestSwapchainColorTarget(VK_FORMAT_B8G8R8A8_SRGB);
+	requestSwapchainDepthBuffer(VK_FORMAT_D32_SFLOAT);
+	requestSwapchainClear(VK_IMAGE_ASPECT_COLOR_BIT | VK_IMAGE_ASPECT_DEPTH_BIT);
+	requestSwapchainImageCount(3);
+	createDevice();
+
 	SDL_ShowWindow(window);
 	//SDL_MaximizeWindow(window);
 
 	while (processWindowEvents())
 	{
-		//beginFrame(); //for example, write camera matrix into the UBO
-		//renderBackdrop();
-		//renderScene();
-		//renderImGui();
-		//endFrame();
+		beginCommandBuffer(eDeviceQueue_Universal);
+
+		RenderPass finalPass = getSwapchainRenderPass();
+		const float clearColor[] = { 0.0f, 0.3f, 0.5f, 1.f };
+		setRenderPassClearColor(finalPass, 0, clearColor);
+		setRenderPassClearDepth(finalPass, 1.f);
+
+		beginRenderPass(finalPass, getSwapchainFramebuffer());
+		endRenderPass();
+
+		submitCommandBuffer(eDeviceQueue_Universal, true);
+
+		presentImageToWindow();
 	}
+
+	deviceWaitIdle();
+	destroyDevice();
 
 	SDL_DestroyWindow(window);
 	return 0;
@@ -50,7 +74,7 @@ SDL_bool processWindowEvents()
 			case SDL_WINDOWEVENT_SIZE_CHANGED:
 				//camera.applyResize(event.window.data1, event.window.data2);
 				//aspectRatio = (float)event.window.data1 / (float)event.window.data2;
-				//resetSwapchain();
+				resetSwapchain();
 				break;
 			}
 			break;
